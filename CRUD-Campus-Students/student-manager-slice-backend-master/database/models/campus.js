8c2e8b18218cdfb18c8e83b242edbed61edc4844
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2020-07-12 15:22:47 -0400

    updated the campuses so it follwed starter

--- CRUD-Campus-Students/student-manager-slice-backend-master/database/models/campus.js
+++ CRUD-Campus-Students/student-manager-slice-backend-master/database/models/campus.js
@@ -1,13 +1,12 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+
+const Sequelize = require("sequelize");
+const db = require("../db");
 
 const Campus = db.define("campus", {
-
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-
+  name: { type: Sequelize.STRING, allowNull: false },
+  address: { type: Sequelize.STRING, allowNull: true },
+  imageUrl: { type: Sequelize.STRING, allowNull: true },
+  description: { type: Sequelize.STRING, allowNull: true },
 });
 
 module.exports = Campus;
--- CRUD-Campus-Students/student-manager-slice-backend-master/routes/campuses.js
+++ CRUD-Campus-Students/student-manager-slice-backend-master/routes/campuses.js
@@ -1,51 +1,114 @@
-const express = require('express');
-const router = express.Router();
-const { Student, Campus } = require('../database/models');
+var express = require("express");
+var router = express.Router();
+const { Campus } = require("../database/models");
 
-/* IN THIS COMMIT, THE DATA THIS ENDPOINT WILL PROVIDE WILL LOOK LIKE SO:
-
-[
-  {
-    "id": 1,
-    "name": "Hunter College",
-    "createdAt": "2020-01-16T19:07:26.525Z",
-    "updatedAt": "2020-01-16T19:07:26.525Z",
-    "students": [
-      {
-        "id": 3,
-        "firstName": "Luka",
-        "lastName": "Doncic",
-        "createdAt": "2020-01-16T19:07:26.539Z",
-        "updatedAt": "2020-01-16T19:07:26.574Z",
-        "campusId": 1
-      },
-      {
-        "id": 2,
-        "firstName": "LeBron",
-        "lastName": "James",
-        "createdAt": "2020-01-16T19:07:26.539Z",
-        "updatedAt": "2020-01-16T19:07:26.569Z",
-        "campusId": 1
-      },
-      {
-        "id": 1,
-        "firstName": "Kyrie",
-        "lastName": "Irving",
-        "createdAt": "2020-01-16T19:07:26.538Z",
-        "updatedAt": "2020-01-16T19:07:26.564Z",
-        "campusId": 1
-      }
-    ]
+/* GET all campuses. */
+// /api/campuses
+router.get("/", async (req, res, next) => {
+  // try to get campuses object from database
+  try {
+    // campuses will be the result of the Campus.findAll promise
+    const campuses = await Campus.findAll();
+    // if campuses is valid, it will be sent as a json response
+    res.status(200).json(campuses);
+  } catch (err) {
+    // if there is an error, it'll passed via the next parameter to the error handler middleware
+    next(err);
   }
-]
-
-*/
-
-router.get('/', function(req, res, next) {
-  Campus.findAll({include: [Student]}) // this is a JOIN;
-    .then(campuses => res.status(200).json(campuses))
-    .catch(err => next(err))
 });
 
-// Export our router, so that it can be imported to construct our apiRouter;
+// Route to serve single campus based on its id
+// /api/campuses/:id
+// /api/campuses/456 would respond with a campus with id 456
+router.get("/:id", async (req, res, next) => {
+  // take the id from params
+  const { id } = req.params;
+  // query the database for a campus with matching id
+  try {
+    // if successful:
+    const campus = await Campus.findByPk(id);
+    // send back the campus as a response
+    res.status(200).json(campus);
+  } catch (err) {
+    // if error:
+    // handle error
+    next(err);
+  }
+});
+
+// Route to handle adding a campus
+// /api/campuses/
+router.post("/", async (req, res, next) => {
+  // Take the form data from the request body
+  const { name, address, description, imageUrl } = req.body;
+  // Create a campus object
+  const campusObj = {
+    name: name,
+    address: address,
+    imageUrl: imageUrl,
+    description: description,
+  };
+  try {
+    // Create a new campus on the database
+    const newCampus = await Campus.create(campusObj);
+    // The database would return a campus
+    // send that campus as a json to the client
+    res.status(201).send(newCampus);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Route to handle editing a campus
+// /api/campuses/:id
+// /api/campuses/456 would modify a campus with id 456
+router.put("/:id", async (req, res, next) => {
+  // get the id from request params
+  const { id } = req.params;
+  // get form data from the request body
+  const { name, address, description, imageUrl } = req.body;
+  const updatedObj = {
+    name: name,
+    address: address,
+    description: description,
+    imageUrl: imageUrl,
+  };
+  try {
+    // if successfull:
+    // Find a campus with a matching id from the database
+    const campus = await Campus.findByPk(id);
+    // database would return a valid campus object or an error
+    console.log(updatedObj);
+    // modify the campus object with new form data
+    await campus.set(updatedObj);
+    // save the new campus object to the data
+    // database would return a new campus object
+    const updatedCampus = await campus.save();
+    console.log(updatedCampus);
+    // send the newCampus as a response from the API
+    res.status(201).send(updatedCampus);
+  } catch (err) {
+    // if error:
+    // handle the error
+    next(err);
+  }
+});
+
+// Route to handle removing a campus
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  // get an id for a campus to delete
+  try {
+    // pass the id to the database to find campus to be deleted
+    // database would either respond succcess or fail
+    const campus = await Campus.findByPk(id);
+    // invoke the .destroy() method on the returned campus
+    await campus.destroy();
+    // send a success message to the client
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
