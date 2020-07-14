77505e7a01cacd3ec29057ba04ee568aa8ae2c39
armankabir <47779642+armankabir@users.noreply.github.com>
2020-07-14 19:27:02 -0400

    Added database values
    Added columns for models of each database. Still need to add association between campus and student

--- CRUD-Campus-Students/Backend/database/models/campus.js
+++ CRUD-Campus-Students/Backend/database/models/campus.js
@@ -4,7 +4,11 @@ const db = require("../db");
 const Campus = db.define("campus", {
   name: { type: Sequelize.STRING, allowNull: false },
   address: { type: Sequelize.STRING, allowNull: true },
-  imageUrl: { type: Sequelize.STRING, allowNull: true },
+  imageUrl: { 
+    type: Sequelize.STRING, 
+    allowNull: true,
+    defaultValue: "https://media.istockphoto.com/vectors/joy-of-college-vector-silhouette-vector-id165762635?k=6&m=165762635&s=612x612&w=0&h=wHZ9BJlKve3NbZcD8RlTRyIAkmOUptiwQT0m0WSTc1A="
+  },
   description: { type: Sequelize.STRING, allowNull: true },
 });
 
---
+++ CRUD-Campus-Students/Backend/database/models/student.js
@@ -0,0 +1,31 @@
+//require sequelize and the database
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Student = db.define("student", {
+    firstName: { type: Sequelize.STRING,  allowNull: true},
+    lastName: { type: Sequelize.STRING, allowNull: true},
+    //Email attributes
+    email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        //require a valid email
+        validate: {isEmail: true},
+    },
+    //imageUrl requires default value
+    imageUrl : {
+        type: Sequelize.STRING, 
+        allowNull: false, 
+        defaultValue: "https://www.watsonmartin.com/wp-content/uploads/2016/03/default-profile-picture.jpg",
+        validate: {isUrl: true}
+    },
+    //gpa must be between 0.0 and 4.0
+    gpa: {
+        type: Sequelize.FLOAT,
+        validate:{
+            isFloat: true,
+            min: 0.0,
+            max: 4.0
+        }
+    },
+});
