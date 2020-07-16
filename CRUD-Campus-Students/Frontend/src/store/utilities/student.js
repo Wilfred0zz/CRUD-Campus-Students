d058aa98fc14bfbc531b23d85b232e20065f21b9
armankabir <47779642+armankabir@users.noreply.github.com>
2020-07-16 06:35:21 -0400

    Initiial Changes to Edit Page
    Edit button is not working when trying to edit a student. May be an issue with passing states as props.

--- CRUD-Campus-Students/Frontend/src/store/utilities/student.js
+++ CRUD-Campus-Students/Frontend/src/store/utilities/student.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // Action Types
 const FETCH_STUDENT = "FETCH_STUDENT";
 const DELETE_STUDENT = "DELETE_STUDENT";
+const EDIT_STUDENT = "EDIT_STUDENT"
 // Action Creators
 
 const fetchStudent = (student) => {
@@ -16,6 +17,13 @@ const deleteStudent = (id) => {
   return{
     type: DELETE_STUDENT,
     payload: id
+  }
+}
+
+const editStudent = (student) => {
+  return{
+    type: EDIT_STUDENT,
+    payload: student,
   }
 }
 
@@ -35,6 +43,13 @@ export const deleteStudentThunk = (id) => (dispatch) => {
   .catch((err) => console.log(err));
 }
 
+export const editStudentThunk = (student) => (dispatch) => {
+  return axios
+    .put(`/api/students/${student.id}`, student)
+    .then(() => dispatch(editStudent(student)))
+    .catch((err) => console.log(err));
+}
+  
 
 // Reducer
 const reducer = (state = [], action) => {
@@ -44,6 +59,11 @@ const reducer = (state = [], action) => {
     case DELETE_STUDENT:
       const newState = [...state];
       return newState.filter((student) => student.id !== action.payload);
+    case EDIT_STUDENT:
+      return state.map((student) =>{
+        if (student.id === action.payload.id) return action.payload;
+        else return student;
+      });
     default:
       return state;
   }
